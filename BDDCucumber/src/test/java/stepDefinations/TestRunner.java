194aaeb419e8447f4c306361035980d18b5bfbbf
Maruthi1989 <bnmaruthiraja@gmail.com>
2024-06-17 19:27:37 +0530

    Initial Commit

---
+++ BDDCucumber/src/test/java/stepDefinations/TestRunner.java
@@ -0,0 +1,14 @@
+package stepDefinations;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/Features/Login.feature", glue= {"stepDefinations"},
+monochrome = true,
+plugin = {"pretty","html:target/cucumber-report.html"})
+public class TestRunner {
+
+}
